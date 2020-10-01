de36229e8ead907c004b856d084bcc99a855e0b2
Romain Goulet <romain.goulet@edu.devinci.fr>
2020-10-01 18:20:57 +0200

    add lilian

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import lilian from '../img/lilian.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={lilian} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Lilian is currently spinning his head right round right round 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Click on his face soon
       </header>
     </div>
   );
