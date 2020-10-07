7bbc7717ddcb1cda14047595131bd1d7c33b8d01
Romain Goulet <romain.goulet@edu.devinci.fr>
2020-10-07 21:08:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,12 +15,12 @@ ReactDOM.render(
     {
       process.env.REACT_APP_API_KEY
     }
-  </div>,
+  </div>
  // </FirebaseContext.Provider>,
-  document.getElementById('root'),
+  //document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
